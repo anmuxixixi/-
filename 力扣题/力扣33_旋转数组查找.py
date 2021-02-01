c44c70aa315b1ac78e0844dd2ce8aea74f33e04a
anmuxixixi <56731178+anmuxixixi@users.noreply.github.com>
2021-02-02 00:16:54 +0800

    Update 力扣33_旋转数组查找.py
    修改代码，代码有误

--- 力扣题/力扣33_旋转数组查找.py
+++ 力扣题/力扣33_旋转数组查找.py
@@ -6,20 +6,25 @@
 
 
 
-def search(nums, target):
+def search(nums，target):
     if not nums:
         return -1
     l, r = 0, len(nums) - 1
-    while l < r:
+    while l <= r:
         mid = (l + r) // 2
         if nums[mid] == target:
             return mid
-        if target < nums[mid]: # 若如目标值处于左边序列中
-            r = mid - 1                   # 缩小范围，二分查找
+        if nums[l] <= nums[mid]:              # 如果左边是有序的
+            if nums[l] <= target < nums[mid]: # 若目标值处于左边序列中
+                r = mid - 1                   # 缩小范围，二分查找
+            else:                             # 目标值处于右边无序的数组中
+                l = mid + 1
         else:
-            l = mid + 1
+            if nums[mid] < target <= nums[len(nums) - 1]:
+                l = mid + 1
+            else:
+                r = mid - 1
     return -1
-
 
 if __name__ == "__main__":
     nums = [4,5,6,7,0,1,2]
